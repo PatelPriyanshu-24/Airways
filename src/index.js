bec63810d04f2de8b4acf67ebb309eb4b9a647ba
PatelPriyanshu-24 <159017504+PatelPriyanshu-24@users.noreply.github.com>
2024-05-24 17:18:41 +0530

    database setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const {PORT}=require('./config/server_config')
 
 const setupAndServer = async()=>{
-
+    app.use(bodyParser.json()); 
+    app.use(bodyParser.urlencoded({extended:true}));
     const app = express();
  
     app.listen(PORT,()=>{
